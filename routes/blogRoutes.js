50b6aafec861c8b0f2c22881172173cb623fdd97
RushikeshTonape <rushikeshtonape143@gmail.com>
2023-06-06 17:31:49 +0530

    Configure route for Blog

---
+++ routes/blogRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+//routes
+
+//GET /all-blog
+router.get('/all-blog' , getAllBlogsController);
+
+//POST /create-blog
+router.post('/create-blog', createBlogController);
+
+//PUT /update-blog
+router.put('/update-blog:id',updateBlogController);
+
+//GET /single blog details
+router.get('/get-blog:id', getBlogByIdController);
+
+//DELETE / blog
+router.delete('/delete-blog:id', deleteBlogController);
