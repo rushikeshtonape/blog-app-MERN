b4c9c3126da83205f72faa2451b42bdaecbfaa26
RushikeshTonape <rushikeshtonape143@gmail.com>
2023-06-19 23:09:56 +0530

    creating getAllBlogs func for getting all blogs

--- client/src/pages/Blogs.js
+++ client/src/pages/Blogs.js
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 const Blogs = () => {
+  const [blogs ,setBlogs] = useState([]);
+  //get all blogs 
+  const getAllBlogs = async ()=>{
+    try{
+    const {data} = await axios.get("/api/v1/blog/all-blog")
+    if(data && data.success){
+      setBlogs(data?.blogs)
+      
+    }
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getAllBlogs();
+  },[])
+
   return (
     <div>Blogs</div>
   )
 }
 
-export default Blogs
+export default Blogs;
