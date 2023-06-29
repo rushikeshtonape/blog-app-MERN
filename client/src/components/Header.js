24cb0537bb8651a7dea5d48e05dd6eeb58058374
RushikeshTonape <rushikeshtonape143@gmail.com>
2023-06-29 19:37:15 +0530

    Completed the all designin part

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,54 +1,90 @@
-import React, { useState } from 'react'
-import {Box , AppBar,Toolbar,Button, Typography,Tabs,Tab} from '@mui/material'
-import { Link, useNavigate } from 'react-router-dom';
-import { useSelector , useDispatch} from 'react-redux';
-import { authActions } from '../redux/store';
-import toast from 'react-hot-toast';
-const Header = () => {
+import React, { useState } from "react";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../redux/store";
+import toast from "react-hot-toast";
 
+const Header = () => {
   //state
-  const [value,setValue] = useState()
+  const [value, setValue] = useState();
 
   //global state
-  let isLogin = useSelector((state)=> state.isLogin)
+  let isLogin = useSelector((state) => state.isLogin);
 
-  isLogin = isLogin || localStorage.getItem('userId');
+  isLogin = isLogin || localStorage.getItem("userId");
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const handleLogout=()=>{
-    toast.success("User Logged Out")
+  const handleLogout = () => {
+    toast.success("User Logged Out");
     dispatch(authActions.logout());
-    navigate('login');
+    navigate("login");
     localStorage.clear();
-  }
+  };
   return (
     <>
-    <AppBar position='sticky'>
+      <AppBar position="sticky">
         <Toolbar>
-            <Typography varient='h1' sx={{fontSize:'30px' , color: "#000" , fontWeight: '500'}}>BLoGGeR</Typography>
-            {isLogin && (
-              <Box display={'flex'} margin={'auto'}>
-              <Tabs textColor='inherit' value={value} onChange={(e,val) => setValue(val)}>
-                  <Tab label='Blogs' LinkComponent={Link} to='/blogs' />
-                  <Tab label='My Blog' LinkComponent={Link} to='/my-blogs' />
-                  <Tab label='Create Blog' LinkComponent={Link} to='/create-blog' />
+          <Typography
+            varient="h1"
+            sx={{ fontSize: "30px", color: "#fff", fontWeight: "500" }}
+          >
+            BLOGGER
+          </Typography>
+          {isLogin && (
+            <Box display={"flex"} margin={"auto"}>
+              <Tabs
+                textColor="inherit"
+                value={value}
+                onChange={(e, val) => setValue(val)}
+              >
+                <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
+                <Tab label="My Blog" LinkComponent={Link} to="/my-blogs" />
+                <Tab
+                  label="Create Blog"
+                  LinkComponent={Link}
+                  to="/create-blog"
+                />
               </Tabs>
-
             </Box>
+          )}
+          <Box display={"flex"} marginLeft={"auto"}>
+            {!isLogin && (
+              <>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/login"
+                >
+                  Login
+                </Button>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/register"
+                >
+                  Registration
+                </Button>
+              </>
             )}
-            <Box display={'flex'} marginLeft={'auto'}>
-                {!isLogin && (<>
-                  <Button sx={{margin:1 , color:'white' }} LinkComponent={Link} to='/login'>Login</Button>
-                <Button sx={{margin:1 , color:'white' }}  LinkComponent={Link} to='/register'>Registration</Button>
-                </>)}
-                {isLogin && (
-                  <Button onClick={handleLogout}  sx={{margin:1 , color:'white' }}>Logout</Button>
-                )}
-            </Box>
+            {isLogin && (
+              <Button onClick={handleLogout} sx={{ margin: 1, color: "white" }}>
+                Logout
+              </Button>
+            )}
+          </Box>
         </Toolbar>
-    </AppBar>
+      </AppBar>
     </>
-  )
-}
+  );
+};
 
 export default Header;
